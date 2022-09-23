b1f2e362371f551c99b5c911176e84ddbe97ac1a
Rodrigo Pereira <rodrigopr404@gmail.com>
2022-09-23 14:49:43 -0300

    🚧 Trabalhando com variáveis

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Olá mundo, Rodrigo")
+	nome := "Rodrigo"
+	var idade = 21
+	var versao float32 = 1.1
+	fmt.Println("Olá mundo, sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("O tipo da variavel nome é", reflect.TypeOf((nome)))
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço é", &comando)
+
+	fmt.Println("O Comando escolhido foi", comando)
+
 }
