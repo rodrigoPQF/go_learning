926afdd41eaf263481ff8f1312458eb65135615c
Rodrigo Pereira <rodrigopr404@gmail.com>
2023-05-22 13:50:56 -0300

    🚧 Aula 3 - Váriaveis, Valores & Tipos - Cap.02

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,11 +2,23 @@ package main
 
 import "fmt"
 
+var y = "strings" // Disponivel em todo o package - não da para usar ":="
+
 func main() {
-	x := 16
-	y := "strings"
+	x := 16 // Disponivel apenas nessa função
+
 	z := true
+
 	_, erros := fmt.Println("Hello World", "Oi galera", 100)
-	fmt.Println(erros)
-	fmt.Println(x, y, z)
+
+	fmt.Println(erros)                // Retorno de { nil }
+	fmt.Printf("x: %v - %T \n", x, x) // $v = value - %T = type
+	fmt.Printf("y: %v - %T \n", y, y)
+	fmt.Printf("z: %v - %T \n", z, z)
+
+	x, w := 20, 30
+
+	fmt.Printf("x: %v - %T \n", x, x)
+	fmt.Printf("x: %v - y: %v", x, w)
+
 }
