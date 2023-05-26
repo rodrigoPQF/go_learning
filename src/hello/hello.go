9a9ee0b170e38f9f7484920463d2f7c6f73141e7
Rodrigo Pereira <rodrigopr404@gmail.com>
2023-05-26 13:14:28 -0300

    🧑‍💻 Condições de corrida

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -799,3 +799,23 @@ func func2() {
 	}
 	wg.Done()
 }
+
+func concorrent2() {
+	contador := 0
+	totaldegoroutines := 10
+	var wg sync.WaitGroup
+	wg.Add(totaldegoroutines)
+
+	for i := 0; i < totaldegoroutines; i++ {
+		go func() {
+			v := contador
+			runtime.Gosched()
+			v++
+			contador = v
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(contador)
+
+}
