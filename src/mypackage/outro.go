73b64f8f8df39545c4c7e17cf4178d9e9732c87e
Rodrigo Pereira <rodrigopr404@gmail.com>
2023-05-29 15:21:27 -0300

    🧑‍💻 Testes no Go

--- src/mypackage/outro.go
+++ src/mypackage/outro.go
@@ -2,6 +2,18 @@ package mypackage
 
 import "fmt"
 
-func Printando() {
+func Printando(a ...interface{}) {
 	fmt.Println("Essa func e main")
+}
+
+func Soma(x int, y int) int {
+	return x + y
+}
+
+func Somas(i ...int) int {
+	total := 0
+	for _, v := range i {
+		total = total + v
+	}
+	return total
 }
---
+++ src/mypackage/outro_test.go
@@ -0,0 +1,41 @@
+package mypackage_test
+
+import (
+	"module/src/mypackage"
+	"testing"
+)
+
+type testes struct {
+	data   []int
+	answer int
+}
+
+func TestSomaEmTabela(t *testing.T) {
+	tests := []testes{
+		{data: []int{1, 2, 3}, answer: 6},
+		{[]int{10, 11, 12}, 33},
+		{[]int{-5, 0, 5, 10}, 10},
+	}
+	for _, v := range tests {
+		z := mypackage.Somas(v.data...)
+		if z != v.answer {
+			t.Error("Expected: ", v.answer, "Got: ", z)
+		}
+	}
+}
+
+func TestSoma(t *testing.T) {
+	teste := mypackage.Somas(3, 2, 1)
+	resultado := 6
+	if teste != 6 {
+		t.Error("Expected: ", resultado, "Got: ", teste)
+	}
+}
+
+func TestSoma2(t *testing.T) {
+	teste := mypackage.Somas(3, 2, 1)
+	resultado := 6
+	if teste != 6 {
+		t.Error("Expected: ", resultado, "Got: ", teste)
+	}
+}
