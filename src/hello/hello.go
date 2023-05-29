0bc3c29ccc3d9789e55622b8141fd0cbe51e5812
Rodrigo Pereira <rodrigopr404@gmail.com>
2023-05-29 11:04:56 -0300

    🧑‍💻 Convergencia

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"module/src/mypackage"
 	"os"
 	"runtime"
@@ -982,4 +983,86 @@ func commaok() {
 
 	v, ok := <-canal
 	fmt.Println(v, ok)
+}
+
+func converg() {
+	par := make(chan int)
+	impar := make(chan int)
+	coverge := make(chan int)
+
+	go envia(par, impar)
+	go receiver2(par, impar, coverge)
+
+	for v := range coverge {
+		fmt.Println("Valor recebido", v)
+	}
+
+}
+func envia(p, i chan int) {
+	x := 100
+	for n := 0; n < x; n++ {
+		if n%2 == 0 {
+			p <- n
+		} else {
+			i <- n
+		}
+	}
+	close(p)
+	close(i)
+}
+
+func receiver2(p, i, c chan int) {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		for v := range p {
+			c <- v
+		}
+		wg.Done()
+
+	}()
+	wg.Add(1)
+	go func() {
+		for v := range i {
+			c <- v
+		}
+		wg.Done()
+
+	}()
+	wg.Wait()
+	close(c)
+}
+
+func maintrabalho() {
+	canal := converge(trabalho("Primeiro"), trabalho("Segundo"))
+
+	for x := 0; x < 15; x++ {
+		fmt.Println(<-canal)
+	}
+}
+
+func trabalho(s string) chan string {
+	canal := make(chan string)
+	go func(s string, c chan string) {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("Função %v diz: %v", s, i)
+			time.Sleep(time.Millisecond * time.Duration(rand.Intn(1e3)))
+		}
+	}(s, canal)
+	return canal
+}
+
+func converge(x, y chan string) chan string {
+	novo := make(chan string)
+	go func() {
+		for {
+			novo <- <-x
+		}
+	}()
+	go func() {
+		for {
+			novo <- <-y
+		}
+	}()
+	return novo
 }
