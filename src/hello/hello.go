a1570ee86ea38231de060fb86a3f60a0009e4807
Rodrigo Pereira <rodrigopr404@gmail.com>
2023-05-29 11:52:46 -0300

    🧑‍💻 Divergencia no go

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1066,3 +1066,81 @@ func converge(x, y chan string) chan string {
 	}()
 	return novo
 }
+
+func divergencia() {
+	canal1 := make(chan int)
+	canal2 := make(chan int)
+	go manda(20, canal1)
+	go outra(canal1, canal2)
+
+	for v := range canal2 {
+		fmt.Println(v)
+	}
+
+}
+func manda(n int, canal chan int) {
+	for i := 0; i < n; i++ {
+		canal <- i
+	}
+	close(canal)
+}
+
+func outra(canal, canal2 chan int) {
+	var wg sync.WaitGroup
+
+	for v := range canal {
+		wg.Add((1))
+		go func(x int) {
+			canal2 <- trabalho2(x)
+			wg.Done()
+		}(v)
+	}
+	wg.Wait()
+	close(canal2)
+}
+func trabalho2(n int) int {
+	time.Sleep(time.Millisecond * time.Duration(rand.Int63n(1e3)))
+	return n
+
+}
+
+func divergencia2() {
+	canal1 := make(chan int)
+	canal2 := make(chan int)
+	functions := 20
+	go manda2(20, canal1)
+	go outra2(functions, canal1, canal2)
+
+	for v := range canal2 {
+		fmt.Println(v)
+	}
+
+}
+func manda2(n int, canal chan int) {
+	for i := 0; i < n; i++ {
+		canal <- i
+	}
+	close(canal)
+}
+
+func outra2(functions int, canal, canal2 chan int) {
+	var wg sync.WaitGroup
+
+	for i := 0; i < functions; i++ {
+		wg.Add((1))
+		go func() {
+			for v := range canal {
+				canal2 <- trabalho2(v)
+			}
+			wg.Done()
+		}()
+		wg.Wait()
+		close(canal2)
+	}
+
+}
+func trabalho3(n int) int {
+	time.Sleep(time.Millisecond * time.Duration(rand.Int63n(1e3)))
+	return n
+
+}
