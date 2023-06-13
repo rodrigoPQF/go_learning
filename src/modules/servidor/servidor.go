7eadbc7976728199bf9aca289303db7e19cdae6a
Rodrigo Pereira <rodrigopr404@gmail.com>
2023-06-13 10:38:05 -0300

    🧑‍💻 Context com testes

---
+++ src/modules/servidor/servidor.go
@@ -0,0 +1,25 @@
+package servidor
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+// Store busca dados
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+}
+
+// Server retorna um handler para chamar a Store
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // todo: registre o erro como você quiser
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
---
+++ src/modules/servidor/servidor_test.go
@@ -0,0 +1,46 @@
+package servidor
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestServer(t *testing.T) {
+	data := "olá, mundo"
+
+	t.Run("retorna dados da store", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if response.Body.String() != data {
+			t.Errorf(`recebi "%s", quero "%s"`, response.Body.String(), data)
+		}
+	})
+
+	t.Run("avisa a store para cancelar o trabalho se a requisicao for cancelada", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := &SpyResponseWriter{}
+
+		svr.ServeHTTP(response, request)
+
+		if response.written {
+			t.Error("uma resposta nao deveria ter sido escrita")
+		}
+	})
+}
